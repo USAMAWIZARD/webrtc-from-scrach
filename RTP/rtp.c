3dfe4fdff851305dbf112c7f89aa9b590956bbf3
notshahzad <shahzadakhtar182005@gmail.com>
2022-12-04 12:51:29 +0630

    fixed i guess

--- RTP/rtp.c
+++ RTP/rtp.c
@@ -1,37 +1,42 @@
-#include<stdlib.h>
-#include<stdio.h>
-struct Rtp {
-   unsigned int v:2;
-   unsigned int padding:1;
-   unsigned int ext:1;
-   unsigned int  csrc_count:4;
-   unsigned int marker:1;
-   unsigned int pt:6;
-   unsigned int seq_no:15; 
-   long int timestamp:32;
-   long int ssrc:32;
-   long int csrc:32;
-   long int hader_ex:32;
-   char *payload; 
+#include <stdio.h>
+#include <stdlib.h>
+struct __attribute__((packed)) Rtp {
+  unsigned char v : 2;
+  unsigned char padding : 1;
+  unsigned char ext : 1;
+  unsigned char csrc_count : 4;
+  unsigned char marker : 1;
+  unsigned char pt : 7;
+  unsigned short int seq_no : 16;
+  long int timestamp : 32;
+  long int ssrc : 32;
+  long int csrc : 32;
+  long int hader_ex : 32;
+  char payload;
 };
 
-struct Rtp*  create_rtp_packet(){
-    static int  seqno=0;
-    struct Rtp * rtp_packet;
-    rtp_packet=(struct  Rtp*) malloc(sizeof(struct Rtp));
-    rtp_packet->v=0;
-    rtp_packet->padding=0;
-    rtp_packet->ext=0;
-    rtp_packet->csrc_count=0;
-    rtp_packet->pt=0;
-    rtp_packet->seq_no=seqno;
-    rtp_packet->timestamp=0;
-    rtp_packet->ssrc=0;
-    rtp_packet->csrc=0;
-    rtp_packet->hader_ex=0;
-    rtp_packet->payload="1";
-    // sprintf("RTP Version: %d, SSRC: %d, Payload Type: %s, Seq Number: %d, CSRC Count: %d, Payload Length: %d Marker: %v",
-	// 	rtp_packet->v, rtp_packet->ssrc, rtp_packet->PayloadType, rtp_packet->SequenceNumber, len(rtp_packet->CSRC), len(p.Payload), rtp_packet->Marker)
-    seqno++;
-    return rtp_packet;
+struct Rtp *create_rtp_packet() {
+  static int seqno = 0;
+  struct Rtp *rtp_packet;
+  rtp_packet = (struct Rtp *)malloc(sizeof(struct Rtp));
+  //mere wireshark me agar \0 hai to age ka data drop karderaha tha to ff rakha sab
+  rtp_packet->v = 3;
+  rtp_packet->padding = 1;
+  rtp_packet->ext = 1;
+  rtp_packet->csrc_count = 0xf;
+  rtp_packet->marker = 1;
+  rtp_packet->pt = 0x3f;
+  rtp_packet->seq_no = 0x7fff;
+  rtp_packet->timestamp = 0xffffffff;
+  rtp_packet->ssrc = 0xffffffff;
+  rtp_packet->csrc = 0xffffffff;
+  rtp_packet->hader_ex = 0xffffffff;
+  rtp_packet->payload = 'a';
+  // sprintf("RTP Version: %d, SSRC: %d, Payload Type: %s, Seq Number: %d,
+  /**CSRC*/
+  // Count: %d, Payload Length: %d Marker: %v", 	rtp_packet->v,
+  // rtp_packet->ssrc, rtp_packet->PayloadType, rtp_packet->SequenceNumber,
+  // len(rtp_packet->CSRC), len(p.Payload), rtp_packet->Marker)
+  seqno++;
+  return rtp_packet;
 }
--- RTP/sendpackets.c
+++ RTP/sendpackets.c
@@ -1,50 +1,51 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 
-// Client side implementation of UDP client-server model 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+// Client side implementation of UDP client-server model
 #include "rtp.c"
-#define PORT     8080 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define PORT 8080
 #define SERVERADDRESS "192.168.0.105" // 138.131.156.36"
+/*#define SERVERADDRESS "127.0.0.1"*/
 
-// Driver code 
-int main() { 
-    int sockfd; 
-    char *hello = "Hello from client"; 
-    struct sockaddr_in   servaddr ; 
-    
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
-    memset(&servaddr, 0, sizeof(servaddr)); 
-        
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = SERVERADDRESS; 
-        
-    int n, len; 
-    struct Rtp *packet =create_rtp_packet();
-    char packet_str[sizeof(struct Rtp)]; 
-    memcpy(packet_str, &packet, sizeof(struct Rtp)); 
-    printf("%s %ld\n",packet_str,sizeof(struct Rtp));
-    sendto(sockfd,packet_str, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
+// Driver code
+int main() {
+  int sockfd;
+  char *hello = "Hello from client";
+  struct sockaddr_in servaddr;
 
+  // Creating socket file descriptor
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-    close(sockfd); 
-    return 0; 
+  memset(&servaddr, 0, sizeof(servaddr));
+
+  // Filling server information
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(PORT);
+  inet_pton(AF_INET, SERVERADDRESS, &(servaddr.sin_addr));
+
+  int n, len;
+  struct Rtp *packet = create_rtp_packet();
+  char packet_str[sizeof(struct Rtp)];
+  memcpy(packet_str, packet, sizeof(struct Rtp));
+  /*printf("%s\n", (char *)&(packet_str[sizeof(struct Rtp)]));*/
+  /*printf("\n");*/
+  sendto(sockfd, packet_str, strlen(packet_str), MSG_CONFIRM,
+         (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+  close(sockfd);
+  return 0;
 }
