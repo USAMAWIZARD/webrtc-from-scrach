9c7b1ff6539d53e5c3e8c2087c1d3b4a0d8526df
notshahzad <shahzadakhtar182005@gmail.com>
2022-12-13 21:20:30 +0630

    payloadfix

--- RTP/rtp.c
+++ RTP/rtp.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct Rtp
-{
+struct Rtp {
   unsigned int v : 2;
   unsigned int padding : 1;
   unsigned int ext : 1;
@@ -17,12 +16,13 @@ struct Rtp
   char payload[];
 };
 
-struct Rtp *create_rtp_packet(char *payload)
-{
+struct Rtp *create_rtp_packet(char *payload) {
   static long int seqno = 0;
   struct Rtp *rtp_packet;
-  rtp_packet = (struct Rtp *)malloc(sizeof(*rtp_packet) + (strlen(payload) * sizeof(char)) + sizeof(char));
-  // mere wireshark me agar \0 hai to age ka data drop karderaha tha to ff rakha sab
+  rtp_packet = (struct Rtp *)malloc(
+      sizeof(*rtp_packet) + (strlen(payload) * sizeof(char)) + sizeof(char));
+  // mere wireshark me agar \0 hai to age ka data drop karderaha tha to ff rakha
+  // sab
   rtp_packet->v = 1;
   rtp_packet->padding = 1;
   rtp_packet->ext = 1;
@@ -35,15 +35,14 @@ struct Rtp *create_rtp_packet(char *payload)
   rtp_packet->csrc = 1;
   rtp_packet->header_ext = 1;
   memcpy(&rtp_packet->payload, payload, strlen(payload) + 1);
-
-
   seqno++;
   return rtp_packet;
 }
-char *convert_rtp_pkt_to_string(struct Rtp *rtp_packet)
-{
-  char *str=NULL;
-  str = (char *)malloc(sizeof(*rtp_packet)); // yaha seg fault aa raha hai
-  sprintf(str, "%d %d %d %d %d %d %d  %ld %ld %ld %ld %s",rtp_packet->v,rtp_packet->padding,rtp_packet->ext,rtp_packet->csrc_count,rtp_packet->marker,rtp_packet->pt,rtp_packet->seq_no,rtp_packet->timestamp,rtp_packet->ssrc,rtp_packet->csrc,rtp_packet->header_ext,rtp_packet->payload);
-  return str;
-}
+/*char *convert_rtp_pkt_to_string(struct Rtp *rtp_packet)*/
+/*{*/
+/*char *str=NULL;*/
+/*str = (char *)malloc(sizeof(*rtp_packet)); // yaha seg fault aa raha hai*/
+/*sprintf(str, "%d %d %d %d %d %d %d  %ld %ld %ld %ld
+ * %s",rtp_packet->v,rtp_packet->padding,rtp_packet->ext,rtp_packet->csrc_count,rtp_packet->marker,rtp_packet->pt,rtp_packet->seq_no,rtp_packet->timestamp,rtp_packet->ssrc,rtp_packet->csrc,rtp_packet->header_ext,rtp_packet->payload);*/
+/*return str;*/
+/*}*/
--- RTP/sendpackets.c
+++ RTP/sendpackets.c
@@ -1,7 +1,4 @@
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
+#include "rtp.c"
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stddef.h>
@@ -11,37 +8,33 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "rtp.c"
 
 #define PORT 8080
-#define SERVERADDRESS "192.168.0.105"
+#define SERVERADDRESS "127.0.0.1"
 
 // Driver code
-int main()
-{
+int main() {
   int sockfd;
   struct sockaddr_in servaddr;
   // Creating socket file descriptor
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-  {
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
     perror("socket creation failed");
     exit(EXIT_FAILURE);
   }
-
   memset(&servaddr, 0, sizeof(servaddr));
-
   // Filling server information
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(PORT);
   inet_pton(AF_INET, SERVERADDRESS, &(servaddr.sin_addr));
 
-  struct Rtp *packet = create_rtp_packet("aaaaaaaaaaaaaaaaagaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaass");
-  char packet_str[34]; 
-
-  memcpy(packet_str, &packet, sizeof(packet_str)+1); 
-  printf("%s",packet);
-  sendto(sockfd, packet_str, sizeof(packet_str) , MSG_CONFIRM,
-         (const struct sockaddr *)&servaddr, sizeof(servaddr));
+  char *payload =
+      "aaaaaaaaaaaaaaaaagaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaass";
+  struct Rtp *packet = create_rtp_packet(payload);
+  char packet_str[sizeof(struct Rtp) - 1 + strlen(payload)];
+  memcpy(packet_str, packet, sizeof(packet_str) / sizeof(packet_str[0]));
+  printf("%s", packet_str + sizeof(struct Rtp));
+  sendto(sockfd, packet_str, sizeof(packet_str) / sizeof(packet_str[0]),
+         MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
   close(sockfd);
   return 0;
