1bafeefba267ccb955194fbfe0f70f1f5e2beebb
USAMAWIZARD <usamawizard@gmail.com>
2022-12-13 23:53:34 +0530

    rtp header format added

--- RTP/rtp.c
+++ RTP/rtp.c
@@ -1,3 +1,19 @@
+
+/*
+0                   1                   2                   3
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|V=2|P|X|  CC   |M|     PT      |       sequence number       |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|                           timestamp                         |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|           synchronization source (SSRC) identifier          |
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+|            contributing source (CSRC) identifiers           |
+|                             ....                            |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,7 +21,7 @@ struct Rtp {
   unsigned int v : 2;
   unsigned int padding : 1;
   unsigned int ext : 1;
-  unsigned int csrc_count : 3;
+  unsigned int csrc_count : 4;
   unsigned int marker : 1;
   unsigned int pt : 7;
   unsigned int seq_no : 16;
