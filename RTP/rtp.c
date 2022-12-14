bccbe164e424d478a799375ab89e384b2d30a11c
notshahzad <shahzadakhtar182005@gmail.com>
2022-12-14 11:09:39 +0630

    spegetticode but atleast it works

--- RTP/rtp.c
+++ RTP/rtp.c
@@ -17,13 +17,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct Rtp {
+struct __attribute__((packed)) Rtp {
+#if G_BYTE_ORDER == G_LITTLE_ENDIAN
+  unsigned int csrc_count : 4;
+  unsigned int ext : 1;
+  unsigned int padding : 1;
+  unsigned int v : 2;
+#elif G_BYTE_ORDER == G_BIG_ENDIAN
   unsigned int v : 2;
   unsigned int padding : 1;
   unsigned int ext : 1;
   unsigned int csrc_count : 4;
+#endif
+#if G_BYTE_ORDER == G_LITTLE_ENDIAN
+  unsigned int pt : 7;
+  unsigned int marker : 1;
+#elif G_BYTE_ORDER == G_BIG_ENDIAN
   unsigned int marker : 1;
   unsigned int pt : 7;
+#endif
   unsigned int seq_no : 16;
   long int timestamp : 32;
   long int ssrc : 32;
@@ -35,11 +47,12 @@ struct Rtp {
 struct Rtp *create_rtp_packet(char *payload) {
   static long int seqno = 0;
   struct Rtp *rtp_packet;
-  rtp_packet = (struct Rtp *)malloc(
-      sizeof(*rtp_packet) + (strlen(payload) * sizeof(char)) + sizeof(char));
+  rtp_packet =
+      (struct Rtp *)malloc(sizeof(*rtp_packet) +
+                           (strlen(payload) * sizeof(char)) + sizeof(char) * 2);
   // mere wireshark me agar \0 hai to age ka data drop karderaha tha to ff rakha
   // sab
-  rtp_packet->v = 1;
+  rtp_packet->v = 2;
   rtp_packet->padding = 1;
   rtp_packet->ext = 1;
   rtp_packet->csrc_count = 0;
@@ -50,7 +63,10 @@ struct Rtp *create_rtp_packet(char *payload) {
   rtp_packet->ssrc = 1;
   rtp_packet->csrc = 1;
   rtp_packet->header_ext = 1;
-  memcpy(&rtp_packet->payload, payload, strlen(payload) + 1);
+  char *newpay = malloc(strlen(payload) + 1);
+  newpay[0] = 0x00;
+  strcpy(newpay + 1, payload);
+  memcpy(&rtp_packet->payload, newpay, strlen(payload));
   seqno++;
   return rtp_packet;
 }
